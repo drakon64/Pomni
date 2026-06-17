8220d3f5da11fc41d77bf4b4dfaac4d81d1ba52e
Evelyn Chance <6444703+drakon64@users.noreply.github.com>
2026-06-17 19:42:47 +0100

    Initial commands

--- src/Program.cs
+++ src/Program.cs
@@ -1,9 +1,37 @@
-﻿namespace pomni;
+﻿using System.CommandLine;
+
+namespace Pomni;
 
 class Program
 {
-    static void Main(string[] args)
+    private static int Main(string[] args)
     {
-        Console.WriteLine("Hello, World!");
+        var rootCommand = new RootCommand("Nix dependency pinning");
+        
+        var initCommand = new Command("init");
+        rootCommand.Add(initCommand);
+        
+        var addCommand = new Command("add");
+        rootCommand.Add(addCommand);
+        
+        var updateCommand = new Command("update");
+        rootCommand.Add(updateCommand);
+        
+        var modifyCommand = new Command("modify");
+        rootCommand.Add(modifyCommand);
+        
+        var removeCommand = new Command("remove");
+        rootCommand.Add(removeCommand);
+        
+        var freezeCommand = new Command("freeze");
+        rootCommand.Add(freezeCommand);
+        
+        var unfreezeCommand = new Command("unfreeze");
+        rootCommand.Add(unfreezeCommand);
+        
+        var botCommand = new Command("bot");
+        rootCommand.Add(botCommand);
+        
+        return rootCommand.Parse(args).Invoke();
     }
 }
