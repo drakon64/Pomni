088273e3edc617f7e611b80034a8eeb4fb36cfd8
Evelyn Chance <6444703+drakon64@users.noreply.github.com>
2026-06-18 10:41:35 +0100

    Whitespace

--- src/Program.cs
+++ src/Program.cs
@@ -7,16 +7,16 @@ class Program
     private static int Main(string[] args)
     {
         var rootCommand = new RootCommand("Nix dependency pinning");
-        
+
         var initCommand = new Command("init");
         rootCommand.Add(initCommand);
-        
+
         var updateCommand = new Command("update");
         rootCommand.Add(updateCommand);
-        
+
         var botCommand = new Command("bot");
         rootCommand.Add(botCommand);
-        
+
         return rootCommand.Parse(args).Invoke();
     }
 }
