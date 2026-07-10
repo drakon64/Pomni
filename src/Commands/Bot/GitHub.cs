db897a30d5e948d445e77eda7c78321d4bcb4ca9
Evelyn Chance <6444703+drakon64@users.noreply.github.com>
2026-07-10 22:50:53 +0100

    Add GitHub URLs to pull request bodies

--- src/Commands/Bot/GitHub.cs
+++ src/Commands/Bot/GitHub.cs
@@ -88,9 +88,13 @@ internal static partial class Bot
 
             foreach (var pin in updatedPins)
                 if (pin.OldRev is not null)
-                    stringBuilder.AppendLine($"- {pin.Pin}: `{pin.OldRev}` -> `{pin.NewRev}`");
+                    stringBuilder.AppendLine(
+                        $"- {pin.Pin}: [{pin.OldRev} -> {pin.NewRev}](https://github.com/{pin.Repository}/compare/{pin.OldRev}...{pin.NewRev})"
+                    );
                 else
-                    stringBuilder.AppendLine($"- {pin.Pin}: init at `{pin.NewRev}`");
+                    stringBuilder.AppendLine(
+                        $"- {pin.Pin}: init at [{pin.NewRev}](https://github.com/{pin.Repository}/tree/{pin.NewRev})"
+                    );
 
             return stringBuilder.ToString();
         }
