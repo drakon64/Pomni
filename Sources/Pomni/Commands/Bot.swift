5e705f6cf685508707ac6f25079532d9817eac95
Evelyn Chance <6444703+drakon64@users.noreply.github.com>
2026-07-23 22:15:44 +0100

    Formatting

--- Sources/Pomni/Commands/Bot.swift
+++ Sources/Pomni/Commands/Bot.swift
@@ -1,7 +1,8 @@
 import ArgumentParser
 
 extension Pomni {
-    struct Bot: ParsableCommand {
-        static let configuration = CommandConfiguration(abstract: "Raise a pull request for pin updates")
-    }
+  struct Bot: ParsableCommand {
+    static let configuration = CommandConfiguration(
+      abstract: "Raise a pull request for pin updates")
+  }
 }
--- Sources/Pomni/Commands/Update.swift
+++ Sources/Pomni/Commands/Update.swift
@@ -1,7 +1,8 @@
 import ArgumentParser
 
 extension Pomni {
-    struct Update: ParsableCommand {
-        static let configuration = CommandConfiguration(abstract: "Update pins to their latest commit or release")
-    }
+  struct Update: ParsableCommand {
+    static let configuration = CommandConfiguration(
+      abstract: "Update pins to their latest commit or release")
+  }
 }
