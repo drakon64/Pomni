89b03d9a13f5260ca45c2666ddfd345ec7dd7153
Evelyn Chance <6444703+drakon64@users.noreply.github.com>
2026-07-23 22:11:20 +0100

    Re-implement `init` command

--- Sources/Pomni/Commands/Init.swift
+++ Sources/Pomni/Commands/Init.swift
@@ -1,7 +1,21 @@
 import ArgumentParser
+import Foundation
 
 extension Pomni {
     struct Init: ParsableCommand {
         static let configuration = CommandConfiguration(abstract: "Create Pomni files")
+
+        mutating func run() throws {
+            let fileManager = FileManager.default
+            let path = fileManager.currentDirectoryPath + "/pomni"
+            try? fileManager.createDirectory(atPath: path, withIntermediateDirectories: false)
+
+            try! "{}".write(toFile: path + "/pomni.lock.json", atomically: true, encoding: .utf8)
+
+            let jsonEncoder = JSONEncoder()
+            jsonEncoder.outputFormatting = .prettyPrinted
+            let pomniJson = try! jsonEncoder.encode(PomniJson(version: 1, pins: [:]).self)
+            fileManager.createFile(atPath: path + "/pomni.json", contents: pomniJson)
+        }
     }
 }
